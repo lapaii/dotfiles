93bc6a788fa60c64d3230569cd18db8e2169b189
lapaii <lapaii@lapaii.dev>
2024-10-10 20:54:43 +0100

    FISH!!!

---
+++ .config/fish/config.fish
@@ -0,0 +1,31 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    oh-my-posh init fish --config $HOME/.config/ohmyposh/mocha.json | source
+
+    zoxide init --cmd cd fish | source
+
+    alias ls='ls --color'
+    alias lsa='ls -a --color'
+    alias clear='clear && catnap'
+    alias grep='grep --color'
+    alias code='vscodium'
+    alias go='cargo-mommy'
+    alias htop='btop'
+    alias YAY='yay'
+    alias YIPPEE='yay'
+
+    export PATH="$HOME/.turso:$PATH"
+    export PATH="$HOME/.cargo/bin:$PATH"
+    export PATH="$HOME/.cargo/env:$PATH"
+    export BUN_INSTALL="$HOME/.bun"
+    export PATH="$BUN_INSTALL/bin:$PATH"
+    export GOPATH="$HOME/Documents/code/go/"
+    export PATH="$GOPATH/bin:$PATH"
+    export PATH="$HOME/.local/bin:$PATH"
+    export CARGO_MOMMYS_ACTUAL="go"
+    export PATH="$HOME/.local/share/fnm:$PATH"
+
+    fnm env | source
+
+    clear
+end
